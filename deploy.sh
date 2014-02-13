3ccf2e4849727f631c9916c8fd80bf14dbb3ac0d
Noah Gibbs <noah_gibbs@yahoo.com>
2014-02-12 16:37:28 -0800

    Add deploy.sh to send content live.

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+git push
+ssh www@codefol.io "RAILS_ENV=production . ~/.bash_profile && cd checkouts/madscience_octopress && git pull && rvm use 1.9.3-p125@deployment && bundle && rake generate && touch tmp/restart.txt"
